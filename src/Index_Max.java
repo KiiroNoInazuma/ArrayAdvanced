7caff9310aa0393c1e32e8f2e925e5657201143e
John Rey <adioskid@mail.ru>
2022-11-05 19:04:38 +0300

    Homework#5_hard

--- src/Index_Max.java
+++ src/Index_Max.java
@@ -2,29 +2,25 @@ import java.util.Arrays;
 
 public class Index_Max {
     public static void main(String[] args) {
-        int check = 0;
-        int index = 0;
+        int numMax = 0;
         int indexMax = 0;
+        int numMin = 0;
+        int indexMin = 0;
         int[] ind = RandomArray.randMet(12, -15, 15);
-        while (index < ind.length) {
-            for (int x : ind) {
-                if (check < x) {
-                    check = x;
-                }
+        for (int x = 0; x < ind.length; x++) {
+            if (numMax <= ind[x]) {
+                numMax = ind[x];
+                indexMax = x;
+            } else if (numMin >= ind[x]) {
+                numMin = ind[x];
+                indexMin = x;
             }
-            if (ind[index] == check) {
-                if (indexMax < index) {
-                    indexMax = index;
-                }
-            }
-            index++;
         }
-        System.out.println("Максимальное число в массиве: " + check);
-        System.out.println("Индекс последнего вхождени в массив: " + indexMax);
+        System.out.println("Максимальное число в массиве: " + numMax);
+        System.out.println("Минимальное число в массиве: " + numMin);
+        System.out.println("Индекс последнего вхождения максимального числа в массив: " + indexMax);
+        System.out.println("Индекс последнего вхождения минимального числа в массив: " + indexMin);
         System.out.println(Arrays.toString(ind));
-
 
     }
 }
-
-/*Создайте массив из 12 случайных целых чисел из отрезка [-15;15]. Определите какой элемент является в этом массиве максимальным и сообщите индекс его последнего вхождения в массив.*/
