b4f2c17404fb38b3e719d1a2f692961d1b571550
John Rey <adioskid@mail.ru>
2022-11-06 15:20:38 +0300

    Homework#5_hard

--- src/FreqElement.java
+++ src/FreqElement.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
 
 public class FreqElement {
+    static boolean bool;
+    static int check;
+    static int count;
+    static int result;
+    static int num;
+
     public static void main(String[] args) {
 
         int[] unit = RandomArray.randMet(12, 0, 1);
-        int check = 0;
-        int count = 0;
-        int result = 0;
-        int num = 0;
+
         while (count < unit.length) {
             for (int x : unit) {
                 if (unit[count] == x) {
@@ -22,7 +25,30 @@ public class FreqElement {
             check = 0;
         }
         System.out.println(Arrays.toString(unit));
-        System.out.println("Число " + num + " встречается чаще всего, в количестве " + result + " раз.");
+
+
+        count = 0;
+        while (count < unit.length) {
+            for (int x : unit) {
+                if (unit[count] == x && unit[count] != num) {
+                    check++;
+                }
+            }
+            count++;
+            if (result == check) {
+                bool = true;
+            }
+            check = 0;
+        }
+        if (result == 1) {
+            System.out.println("В массиве нет повторяющихся чисел");
+        } else {
+            if (bool) {
+                System.out.println("В массиве есть несколько одинаковых чаще повторяющихся чисел");
+            } else {
+                System.out.println("Число " + num + " встречается чаще всего, в количестве " + result + " раз.");
+            }
+        }
     }
 }
 
